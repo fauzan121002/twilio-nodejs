152828d742f01fdc21d6acf2d05392dd1644975d
Muhammad Fauzan <50759463+fauzan121002@users.noreply.github.com>
2020-01-23 14:15:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const twilio = require('twilio')
 
-const accountSid = 'AC7d16389466cf0423ea7bc80eda5f1ac7',
-	  authToken = '7b436017ed9ad4f4d09ff00ff76229a2'
+const accountSid = 'xxxxxxxxxxxx',
+	  authToken = 'xxxxxxxxxxx'
 
 const client = new twilio(accountSid, authToken)
 
@@ -9,8 +9,8 @@ client.messages.create({
 	body: `
 		Example Message	
 	`,
-	to: '+6281293287123',
-	from: '+00000000000'
+	to: '+xxxxxxx',
+	from: '+xxxxxxxx'
 })
 .then((message) => console.log(message))
 .catch((err) => console.log(err))
